1c9cab2533f61f6a7880d665d128402b2764b458
NaveenSuresh07 <naveensuresh.07ns@gmail.com>
2026-02-24 23:35:52 +0530

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-    username: String,
-    email: { type: String, unique: true },
-    interests: [String],
-    notes: [{ content: String, summary: String }]
-});
-module.exports = mongoose.model('User', UserSchema);
